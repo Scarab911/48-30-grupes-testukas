d24dfb581a4dac06f058f4cc8d454de3c64a7cea
Paulius B <paulspremium@gmail.com>
2021-08-12 19:29:13 +0300

    all logs success

--- index.js
+++ index.js
@@ -20,13 +20,98 @@ const data = [
 // PASTABA: jei kazkuri reikalavima atitinka daugiau nei vienas mokinys, spausdinti visus
 
 // Isspausdinti mokinio varda ir pazymi, kuris turi prasciausia pazymi
+let lowestMark = 10;
+let bigestMark = 0;
+let averageMarksList = [];
+
+for (const { name, marks } of data) {
+    let sum = 0;
+    for (const number of marks) {
+
+        sum += number;
+
+        if (number < lowestMark) {
+            lowestMark = number;
+        } else if (number > bigestMark) {
+            bigestMark = number;
+        }
+    }
+
+    averageMarksList.push(sum / marks.length)
+
+    if (marks.includes(lowestMark)) {
+        console.log(`Maziausia pazymi ${lowestMark} turi mokinys:`, name);
+    }
+
+    if (marks.includes(bigestMark)) {
+        console.log(`Didziausia pazymi ${bigestMark} turi mokinys:`, name);
+    }
+}
 
 // Isspausdinti mokinio varda ir pazymi, kuris turi geriausia pazymi
 
-// Isspausdinti mokinio varda ir pazymiu vidurki, kuris turi prasciausia pazymiu vidurki
+// let bigestMark = 0;
+// for (const { name, marks } of data) {
+//     for (const number of marks) {
 
+//         if (number > bigestMark) {
+//             bigestMark = number;
+//         }
+//     }
+//     if (marks.includes(bigestMark)) {
+//         console.log(`Didziausia pazymi ${bigestMark} turi mokinys:`, name);
+
+//     }
+// }
+
+// Isspausdinti mokinio varda ir pazymiu vidurki, kuris turi prasciausia pazymiu vidurki
 // Isspausdinti mokinio varda ir pazymiu vidurki, kuris turi geriausia pazymiu vidurki
+let lowestAverage = 10;
+let highestAverage = 0;
+
+for (let i = 0; i < averageMarksList.length; i++) {
+    const average = averageMarksList[i];
+
+    if (average > highestAverage) {
+        highestAverage = average;
+    }
+
+    if (average < lowestAverage) {
+        lowestAverage = average;
+    }
+}
+
+for (let i = 0; i < averageMarksList.length; i++) {
+    const average = averageMarksList[i];
+
+    if (average === lowestAverage) {
+        console.log(`Maziausia vidurki ${lowestAverage} turi ${data[i].name}`);
+    }
+    if (average === highestAverage) {
+        console.log(`Didziausia vidurki ${highestAverage} turi ${data[i].name}`);
+    }
+}
+
+
 
 // Isspausdinti mokinio varda, jei jis turi pazymi 8
+for (const { name, marks } of data) {
 
+    for (const number of marks) {
+
+        if (number === 8) {
+            console.log(`Astuoneta turi: ${name}`);
+        }
+    }
+}
 // Isspausdinti mokinio varda, jei jis neturi pazymio 7
+for (const { name, marks } of data) {
+
+    for (const number of marks) {
+
+        if (marks.includes(7) === false) {
+            console.log(`Septyneto neturi: ${name}`);
+            return
+        }
+    }
+}
